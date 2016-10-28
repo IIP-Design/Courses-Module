f908445c304fc305bbd0e5090ae1d50fa5a31b22
Terri Regan <contact@terriregan.com>
2016-10-28 15:19:08 -0400

    Updates actions and types

--- app/src/actions/actions.js
+++ app/src/actions/actions.js
@@ -1,20 +1,62 @@
-import * as types from '../actions/types';
+const axios = require('axios');
+const types = require('../actions/types');
 
-export function fetchQuiz(data) {
+// utility method
+function getData(data) {
+  var keys = Object.keys(data);
+  var result = keys.map((key) => {
+    return data[key];
+  })
+  return result;
+}
+
+// let app know data being fetched
+export function fetchRequest() {
   return {
-    type: types.FETCH_QUIZ,
+    type: types.FETCH_REQUEST,
     payload: {
-    	questions: data[0].lessons[0].quiz
+      isFetching: true
     }
   };
 }
 
-export function fetchQuizError(error) {
+// let app know data error occurred during fetch
+export function fetchError(error) {
   return {
-    type: types.FETCH_QUIZ_ERROR,
+    type: types.FETCH_ERROR,
     payload: {
-    	error: error
+      error: error,
+      isFetching: false
     }
   };
 }
 
+// set current lesson
+export function setLesson(lesson) {
+ return {
+    type: types.SET_LESSON,
+    payload: {
+      detail: lesson,
+      image: lesson.image,
+      media: lesson.media,
+      glossary: lesson.glossary,
+      resources: lesson.resources
+    }
+  };
+}
+
+// populate redux store upon successful fetch
+export function fetchCourseComplete(data) {
+  let course = data.entities.course[data.result];  // get course by id
+  
+  return {
+    type: types.FETCH_COURSE_COMPLETE,
+    payload: {
+      isFetching: false,
+      detail: course,
+      lessons: getData(data.entities.lessons),
+      instructors: getData(data.entities.instructors),
+      quiz: getData(data.entities.quiz)
+    }
+  };
+}
--- app/src/actions/types.js
+++ app/src/actions/types.js
@@ -1,7 +1,11 @@
-// Quiz
-export const FETCH_QUIZ = 'FETCH_QUIZ';
-export const FETCH_QUIZ_ERROR = 'FETCH_QUIZ_ERROR';
-
 // Course
-export const FETCH_COURSES = 'FETCH_COURSES';
-export const FETCH_COURSE = 'FETCH_COURSE';
+export const FETCH_COURSES_COMPLETE 	= 'FETCH_COURSES_COMPLETE';
+export const FETCH_COURSE_COMPLETE 		= 'FETCH_COURSE_COMPLETE';
+
+// Pages
+export const SET_LESSON 							= 'SET_LESSON';
+export const FETCH_QUIZ 							= 'FETCH_QUIZ';
+
+// Common 
+export const FETCH_REQUEST 						= 'FETCH_REQUEST';
+export const FETCH_ERROR 							= 'FETCH_ERROR';
