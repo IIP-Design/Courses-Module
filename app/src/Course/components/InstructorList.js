93f28353d5899e5a70a3021260310a6e18cf98ed
Edwin Mah <me@edwinmah.com>
2018-04-05 13:51:50 -0400

    Remove unused imports

--- app/src/Course/components/InstructorList.js
+++ app/src/Course/components/InstructorList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import shortid from 'shortid';
-import MediaObject from 'App/components/MediaObject';
 
 require('Course/components/stylesheets/InstructorList.scss');
 
--- app/src/Quiz/reducers.js
+++ app/src/Quiz/reducers.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import * as types from './actionTypes';
 
 const initialState = {
