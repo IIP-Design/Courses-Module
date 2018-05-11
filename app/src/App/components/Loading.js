21f5f28ef3611df7eb6e28b0ee58f25603de6e0d
Edwin Mah <me@edwinmah.com>
2018-05-11 16:52:41 -0400

    Add basic styling to Loading component

--- app/src/App/components/Loading.js
+++ app/src/App/components/Loading.js
@@ -9,12 +9,23 @@ import React from 'react';
  */
 
 const Loading = (props) => {
+  const innerWidth = (window.innerWidth > 620) ? '62.5%' : 'initial';
+  const style = {
+    maxWidth: innerWidth,
+    marginTop: '2em',
+    marginBottom: '2em'
+  };
+
   if (props.error) {
-    return <div>Error!</div>;
+    return (
+      <p style={ style }>
+        Sorry, this course didn&rsquo;t load for some reason. If you&rsquo;re accessing this course from a mobile device, try a desktop or laptop computer. Please contact us if you continue to have difficulty accessing this course.
+      </p>
+    );
   } else if (props.timedOut) {
-    return <div>Taking a long time...</div>;
+    return <p style={ style }>Taking a long time...</p>;
   } else if (props.pastDelay) {
-    return <div>Loading...</div>;
+    return <p style={ style }>Loading...</p>;
   }
   return null;
 };
